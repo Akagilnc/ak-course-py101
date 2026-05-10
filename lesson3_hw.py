24e8e6f6d11d6251fffabfe750d2047b2163724a
Akagi <akagilnc@gmail.com>
2026-05-10 14:32:25 +0900

    chore: clean python course repository

--- lesson3_hw.py
+++
@@ -1,7 +0,0 @@
-spam = {'name': 'Curry',
-        'age': '32',
-        'kids': [{'name': 'Riley'}, {'name': 'Canon'}, {'name': 'Ryan'}]}
-
-infos = spam.get('kids')
-for kid in infos:
-    print(kid.get('name'))
--- lesson4.py
+++
@@ -1,7 +0,0 @@
-spam = {'name': 'Curry', 'age': '32',
-        'kids': [{'name': 'Riley'}, {'name': 'Canon'}, {'name': 'Ryan'}]}
-
-data = spam.get('kids')
-for kid in data:
-    print(kid.get('name'))
-
